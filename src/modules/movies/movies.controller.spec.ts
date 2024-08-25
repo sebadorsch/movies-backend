124099113fa6cb905af81710956d0e43684ac969
sebastian <sebastian.dorsch@crombie.dev>
2024-08-24 22:35:43 -0300

    vercel.json

--- src/modules/movies/movies.controller.spec.ts
+++ src/modules/movies/movies.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-import { PrismaService } from '../prisma/prisma.service';
 
 describe('MoviesController', () => {
   let controller: MoviesController;
