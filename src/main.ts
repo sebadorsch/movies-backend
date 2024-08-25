2dfec86091dca225b1e2d8a58f168db4593d9ca3
sebastian <sebastian.dorsch@crombie.dev>
2024-08-25 01:41:54 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,6 @@ import * as process from 'node:process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
